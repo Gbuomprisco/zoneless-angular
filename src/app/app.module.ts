a76b861afab56eaf6ac524d4a94830739b9161ce
giancarlo <giancarlopsk@gmail.com>
2019-11-16 17:28:26 +0000

    Removed Old Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CommonModule } from '@angular/common';
-import { CounterComponent } from './counter/counter.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CounterComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
